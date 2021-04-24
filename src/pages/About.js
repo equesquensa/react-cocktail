bf3172d76c33efadbfc1646a9feec2f9910198a0
Eduardo Quesquen <equesquensa@gmail.com>
2021-04-24 14:26:22 -0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<section className='section about-section'>
+			<h1 className='section-title'>about us</h1>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium rem
+				cum repudiandae id minima odit illum, dolores optio voluptatem expedita
+				a eligendi iste? Iste aspernatur reiciendis neque cum quod magni fugiat,
+				doloribus aperiam, eligendi ut dolores eum? Autem, deleniti nisi!
+			</p>
+		</section>
+	);
+};
+
+export default About;
